bc389ec2057f436a42aa0cc1e667e07d74352c29
Gulshan06 <gullukr2132@gmail.com>
2021-07-26 20:03:34 +0530

    day5 work

---
+++ day5assignment-23july/26july2021-Gulshan-day5-Assigment/students.py
@@ -0,0 +1,8 @@
+data = [{'name':'ram',"rollno":25,"admin_num":3005,"college":'RJIT',"marks":{'eng':50,"maths":87,"science":89}},{'name':'Rohit',"rollno":26,"admin_num":3006,"college":'RJIT',"marks":{'eng':60,"maths":77,"science":89}},{'name':'Abhi',"rollno":27,"admin_num":305,"college":'RJIT',"marks":{'eng':70,"maths":37,"science":90}},{'name':'sita',"rollno":28,"admin_num":5605,"college":'RJIT',"marks":{'eng':40,"maths":87,"science":89}},{'name':'shyam',"rollno":29,"admin_num":3011,"college":'RJIT',"marks":{'eng':50,"maths":77,"science":89}}]
+# print(data)
+# print(len(data))
+# print(data[0])
+# print(data[0].values())
+print(data[0]["marks"])
+
+
