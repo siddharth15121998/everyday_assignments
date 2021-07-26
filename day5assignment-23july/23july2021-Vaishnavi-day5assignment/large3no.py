6227fd02fe70d82c4121d08f58f76c6f1359ac51
Vaishnavi Hajela <65701834+Vaishnavi6520@users.noreply.github.com>
2021-07-26 23:20:23 +0530

    day5assignment-23july

---
+++ day5assignment-23july/23july2021-Vaishnavi-day5assignment/large3no.py
@@ -0,0 +1,9 @@
+a=int(input("enter a number is a : "))
+b=int(input("enter a number is b : "))
+c=int(input("enter a number is c : "))
+if(a>b and a>c):
+    print("a is larget")
+elif(b>c and b>a):
+    print("b is largest")
+else:
+    print("c is largest")
---
+++ day5assignment-23july/23july2021-Vaishnavi-day5assignment/mathx.py
@@ -0,0 +1,7 @@
+import math
+a =int(input("Enter a number: ")) 
+print(math.factorial(a))
+
+import math 
+a =float(input("Enter a number: "))   
+print(math.floor(a))  
