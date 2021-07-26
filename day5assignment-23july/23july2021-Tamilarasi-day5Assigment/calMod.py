f5d46c30960a9604ab347ee33b79fd82c32033b1
tamilarasikumar <tamilarasiifet@gmail.com>
2021-07-26 21:56:07 +0530

    day5-6 completed

---
+++ day5assignment-23july/23july2021-Tamilarasi-day5Assigment/calMod.py
@@ -0,0 +1,11 @@
+import func as operation
+a=int(input("enter 1st no.:"))
+b=int(input("enter 2nd no.: "))
+print(operation.add(a,b))
+print(operation.sub(a,b))
+print(operation.mul(a,b))
+print(operation.div(a,b))
+print(operation.floor(a,b))
+
+
+
