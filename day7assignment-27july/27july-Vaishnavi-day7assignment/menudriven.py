9ce641bd43506c26d957f2d7efdb16176f6014b3
Vaishnavi Hajela <65701834+Vaishnavi6520@users.noreply.github.com>
2021-07-27 23:57:19 +0530

    day7-assignment

---
+++ day7assignment-27july/27july-Vaishnavi-day7assignment/menudriven.py
@@ -0,0 +1,23 @@
+while(True):
+    print("Select an option from menu ")
+    print("\n")
+    print("1.addition")
+    print("2.subtraction")
+    print("3.multiplication")
+    print("4.division")
+    print("5.exit")
+    choice=int(input("Enter the choice :"))
+    if choice==1:
+        print("Addition selected")
+
+    if choice==2:
+        print("Subtraction selected")
+
+    if choice==3:
+        print("Multiplication selected")
+
+    if choice==4:
+        print("Division selected")
+
+    if choice==5:
+        break
---
+++ day7assignment-27july/27july-Vaishnavi-day7assignment/whileloopx.py
@@ -0,0 +1,10 @@
+i=int(input("Enter a number to start :"))
+e=int(input("Enter a number to end :"))
+while (i<=e):
+    print(i,end=' ')
+    i=i+1
+
+# i=1
+# while(i<=10):
+#     print(i)
+#     i=i+1
